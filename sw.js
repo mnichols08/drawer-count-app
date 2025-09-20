2fbf8388a9fc278068c30be5d305baf66abf7add
Mikey Nichols <mnix@journeytocode.io>
2025-09-20 16:25:20 -0400

    bump CACHE_VERSION to v12 in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Drawer Count Service Worker */
-const CACHE_VERSION = 'v11';
+const CACHE_VERSION = 'v12';
 const PRECACHE = `precache-${CACHE_VERSION}`;
 const RUNTIME = `runtime-${CACHE_VERSION}`;
 
