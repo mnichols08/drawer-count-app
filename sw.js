39994917a33bbd31268638aca91eb65b94ef9b9e
Mikey Nichols <mnix@journeytocode.io>
2025-09-22 15:55:59 -0400

    chore(release): bump to v0.2.21

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Drawer Count Service Worker */
-const CACHE_VERSION = 'v0.2.20';
+const CACHE_VERSION = 'v0.2.21';
 const PRECACHE = `precache-${CACHE_VERSION}`;
 const RUNTIME = `runtime-${CACHE_VERSION}`;
 
